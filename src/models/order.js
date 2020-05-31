e827cbc44f26b38ec610ab90b86c2e00411b6f98
Ngô Thanh Sơn <songas000@gmail.com>
2020-06-01 03:07:20 +0700

    Config paypal data

--- src/models/order.js
+++ src/models/order.js
@@ -4,6 +4,55 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const OrderSchema = new mongoose.Schema({
+    online: {
+        type: Boolean
+    },
+    // các dạng status: 'Waiting for Phone Comfirm, Prepraring your order, Delivering..., Completed'
+    status: {
+        type: String,
+        default: 'Waiting for Phone Comfirm'
+    },
+    finalprice: {
+        type: Number
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    items: [{
+        name: {
+            type: String,
+            default: 'Unknown'
+        },
+        quantity: {
+            type: Number,
+            default: 1
+        },
+        price: {
+            type: Number
+        },
+        dish: {
+            type: mongoose.Schema.Types.ObjectId,
+
+        }
+    }],
+    paymentmethod: {
+        paymentname: {
+            type: String
+        },
+        paymentid: {
+            type: String
+        },
+        payer:{
+            name:{
+                type: String
+            },
+        },
+        status: {
+            type: String,
+            default: 'Uncomplete'
+        }
+    }
 },{
     timestamps: true
 })
--- src/routers/checkout.js
+++ src/routers/checkout.js
@@ -3,36 +3,88 @@ const router = new express.Router()
 
 const auth = require('../middleware/auth')
 
+const userTable = require('../models/usertable')
+const Dish = require('../models/dish')
+
 const paypal = require('@paypal/checkout-server-sdk')
 const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
 const payPalClient = require('../utils/paypalClient')
+const {shippingfee} = require('../utils/getWebdata')
 
 //hàm thanh toán (client gọi CreateOrder và gửi data bàn ăn lên)
 //server => lấy data usertable theo id và cập nhật vào data order
-router.post('/users/me/table/:tableid/checkout',auth, async (req, res) => {
+router.post('/users/me/table/:tableid/checkout',auth , async (req, res) => {
     console.log('checkout running')
 
-    console.log('USER ID: ' + req.user._id)
-    console.log('TABLE ID: '+ req.params.tableid)
+    // console.log('USER ID: ' + req.user._id)
+    // console.log('TABLE ID: '+ req.params.tableid)
+    // 1. Find table and construct paypal data
 
-    // 2. Allow cross-domain
-    res.setHeader('access-control-allow-origin', '*');
+    const table = await userTable.findOne({
+        userid : req.user._id, 
+       // _id : req.body.table
+    })
+    
+    // run foreach và tìm data đồ vào data paypal
+    const items = []
+    var counter = 0
 
-     // 3. Call PayPal to set up a transaction
-        const request = new paypal.orders.OrdersCreateRequest();
-        request.prefer("return=representation");
-        request.requestBody({
+    //để async foreach chạy phải để trong promises
+    const code = new Promise(function(resolve, reject) {
+        table.dishes.forEach(async element => {
+            console.log('foreach running');
+            
+            const pdish = await Dish.findById(element.dish)
+            items.push({
+                name: pdish.name,
+                description: pdish.description,
+                unit_amount: {
+                    currency_code: 'USD',
+                    value: pdish.promotionprice
+                },
+                quantity: element.quantity
+            })
+            counter ++
+            if(counter === table.dishes.length) {
+                resolve()
+            }
+        })
+    })
+    
+    await code.then(async () => {
+        const paypaldata = {
             intent: 'CAPTURE',
             purchase_units: [{
-            amount: {
-                currency_code: 'USD',
-                value: '220.00'
-            }
-            }]
-        });
+                amount: {
+                    currency_code: 'USD',
+                    value: table.totalprice + shippingfee,
+                    breakdown: {
+                        item_total:{
+                            currency_code: 'USD',
+                            value: table.totalprice
+                        },
+                        shipping: {
+                            currency_code: 'USD',
+                            value: shippingfee
+                        }
+                    }
+                },
+                items: items
+            }],
+        }
+
+        
+        // 2. Allow cross-domain
+        res.setHeader('access-control-allow-origin', '*');
+
+        // 3. Call PayPal to set up a transaction
+        const request = new paypal.orders.OrdersCreateRequest();
+        request.prefer("return=representation");
+        request.requestBody(paypaldata);
 
         let order;
         try {
+            console.log('order running');
             order = await payPalClient.client().execute(request);
         } catch (err) {
 
@@ -40,11 +92,14 @@ router.post('/users/me/table/:tableid/checkout',auth, async (req, res) => {
             console.error(err);
             return res.send(500);
         }
+        // 5. Tạo 1 order mới và lưu lại
 
-        // 5. Return a successful response to the client with the order ID
+        // 6. Return a successful response to the client with the order ID
         res.status(200).json({
             orderID: order.result.id
         });
+    })
+
 })
 
 module.exports = router
